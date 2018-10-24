78357a21325d369221a52bff5a275402c9584e5a
Awal_Hasan <vsangan13awal@gmail.com>
2018-10-24 08:59:41 +0700

    Add files via upload
    Script 1

---
+++ run.zsh
@@ -0,0 +1,14 @@
+#! /bin/zsh
+# Myscript First 
+#! Say Hello
+
+echo "Hello,What's your name??" |lolcat
+read a
+echo "Welcome Mr./Mrs.$a, How are you fell??" | lolcat
+read b
+echo "I'm happy cause your $b :)" |lolcat
+
+echo "\n**********************"|lolcat
+echo "Thanks Mr./Mrs. $a for visited " | lolcat
+echo "**********************"|lolcat
+echo "Good bye Mr./Mrs. $a " | lolcat
