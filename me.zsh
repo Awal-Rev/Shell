f0748f7e409b6cd2b81a3c42f890e26a4481f3ac
Awal_Hasan <vsangan13awal@gmail.com>
2018-10-24 09:45:06 +0700

    Add files via upload
    Script 2

---
+++ me.zsh
@@ -0,0 +1,17 @@
+#! /bin/zsh
+# Biodata
+
+echo "Hello, What your name ?"
+read a
+echo "How old are you ?"
+read b
+echo "What is your hobby ?"
+read c
+
+echo "Thank you Mr./Mrs.$a\n "
+echo "Your name is $a\nYour old is : ${b} years old \nAnd your hobby is ${c}ing\n"
+
+echo "Good bye Mr./Mrs. $a it's time to say you good bye" |lolcat 
+
+
+
