726144fcc5e18bc9743b4e7bb36730b6115f5298
Awal_Hasan <vsangan13awal@gmail.com>
2018-10-27 21:55:59 +0700

    Add files via upload
    calculator  v2

--- calculator.sh
+++ calculator.sh
@@ -1,83 +1,70 @@
 #! /bin/bash
 
-printf "%25s\n" | tr " " -
-echo "      Calculatorku" | lolcat
-printf "%25s\n" | tr " " -
-echo -e "Pertambahan (a) | Perkalian (b) | Perkurangan (c) | Pembagian (d) " |lolcat
-echo "Pilih Menu Perhitungan (a/b/c/d)"
-read -p "Menu = " cal 
+pertambahan=Pertambahan
+perkalian=Perkalian
+pembagian=Pembagian
+perkurangan=Perkurangan
+
+printf "%39s\n" | tr " " = | lolcat 
+echo "|            Calculatorku             |" | lolcat
+printf "%39s\n" | tr " " = | lolcat
+echo -e "| Pertambahan (a) |    Perkalian (b)  |" | lolcat
+echo -e "| Perkurangan (c) |    Pembagian (d)  |" | lolcat
+echo -e "|         |  Campuran (e)  |          | " | lolcat
+printf "%39s\n" | tr " " = | lolcat
+echo  "| Pilih Menu Perhitungan [a/b/c/d/e]  |" | lolcat 
+echo "|=====================================|" | lolcat
+read -p "| Masukkan Menu = " cal
 if [ $cal == a ]; then 
-    echo "Masukkan Angka : "
-    read e
-    echo "Masukkan Angka : "
-    read f
-        echo "Hasil= $[e+f]"
+
+    read -p "| Masukkan Angka : " e
+    read -p "| Masukkan Angka : " f
+        printf "%39s\n" | tr " " = | lolcat
+            echo "| Hasil $pertambahan  | $[e+f]            |" | lolcat
+        printf "%39s\n" | tr " " = | lolcat    
 fi
 if  [ $cal == b ]; then
-    echo "Masukkan Angka : "
-    read g
-    echo "Masukkan Angka : "
-    read h
-        echo "Hasil= $[g*h]"
+    read -p "| Masukkan Angka : " g
+    read -p "| Masukkan Angka : " h
+        printf "%39s\n" | tr " " = | lolcat
+            echo "| Hasil $perkalian    | $[g*h]            |" | lolcat
+        printf "%39s\n" | tr " " = | lolcat    
 fi
 if  [ $cal == c ]; then
-    echo "Masukkan Angka : "
-    read i
-    echo "Masukkan Angka : "
-    read j
-        echo "Hasil= $[i-j]"
+    read -p "| Masukkan Angka : " i
+    read -p "| Masukkan Angka : " j
+        printf "%39s\n" | tr " " = | lolcat
+            echo "Hasil $perkurangan    | $[i-j]            |" | lolcat
+        printf "%39s\n" | tr " " = | lolcat    
 fi
 if [ $cal == d ]; then
-    echo "Masukkan Angka : "
-    read k
-    echo "Masukkan Angka : "
-    read l
-        echo "Hasil= $[k/l]"
+    read -p "| Masukkan Angka : " k
+    read -p "| Masukkan Angka : " l
+        printf "%39s\n" | tr " " = | lolcat
+            echo "Hasil $pembagian      | $[k/l]            |" | lolcat
+        printf "%39s\n" | tr " " = | lolcat    
 fi
+if [ $cal == e ]; then
+    read -p "| Masukkan Angka : " m
+    read -p "| Masukkan Angka : " n
+        printf "%39s\n" | tr " " = | lolcat
+            echo "| Hasil $pertambahan  | $[m+n]            |" | lolcat
+            echo "| Hasil $perkalian    | $[m*n]          |" | lolcat
+            echo "| Hasil $perkurangan  | $[m-n]              |" | lolcat
+            echo "| Hasil $pembagian    | $[m/n]              |" | lolcat
+fi    
 
-echo -e "Mau Keluar (y/n)"
-read o
+read -p "| Mau Keluar (y/n) " o
 if [ $o == y ]; then
-    echo "Terima kasih sudah memakai program ini"
+printf "%40s\n" | tr " " = | lolcat
+    echo "|        Program by: Awal Hasan        |" | lolcat
+    echo "|Terima kasih sudah memakai program ini|" | lolcat
+printf "%40s\n" | tr " " = | lolcat 
     exit
 fi
 if [ $o == n ]; then
-    echo "Oke, Tunggu beberapa saat"
+    echo "Tunggu Beberapa Saat"
+    printf "%25s\n"
+    ./calculator.sh
+    exit
 fi    
-echo -e "Pertambahan (a)   Perkalian (b)  Perkurangan (c)  Pembagian (d) " |lolcat
-echo "Pilih Menu Perhitungan (a/b/c/d)"
-read -p "Menu = " cal 
-if [ $cal == a ]; then 
-    echo "Masukkan Angka : "
-    read e
-    echo "Masukkan Angka : "
-    read f
-        echo "Hasil= $[e+f]"
-fi
-if  [ $cal == b ]; then
-    echo "Masukkan Angka : "
-    read g
-    echo "Masukkan Angka : "
-    read h
-        echo "Hasil= $[g*h]"
-fi
-if  [ $cal == c ]; then
-    echo "Masukkan Angka : "
-    read i
-    echo "Masukkan Angka : "
-    read j
-        echo "Hasil= $[i-j]"
-fi
-if [ $cal == d ]; then
-    echo "Masukkan Angka : "
-    read k
-    echo "Masukkan Angka : "
-    read l
-        echo "Hasil= $[k/l]"
-fi
-
-printf "%30s\n" | tr " " -
-echo "Terima Kasih sudah menggunakan produk ini :)"  
-printf "%30s\n" | tr " " -0
-
-
